28f79b20028193980788d77efb5e2959301ead62
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2025-08-02 00:27:15 -0700

    skater can now rotate around poles!

--- scenes/skater.gd
+++ scenes/skater.gd
@@ -1,18 +1,51 @@
 extends CharacterBody2D
 
-const SPEED := 250
+const SPEED := 250.0
+const VECTOR_CHANGE_FACTOR := 250.0
 
-@export_range(0, 2 * PI) var initial_direction_angle := 0.0
-@onready var direction_angle := initial_direction_angle
+@onready var hook_detector: HookDetector = $HookDetector
+@onready var hook_connector: Line2D = $HookConnector
+
+@export_range(0, 2 * PI) var start_direction_angle := 0.0
+@onready var direction_vector := Vector2.RIGHT.rotated(start_direction_angle)
 var is_started := false
+var connected_hook_position := Vector2.ZERO
+var connected_hook_distance := 0.0
+var is_connected_to_hook := false
+var is_rotating_clockwise := false
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("control_skater") and not is_started:
-		is_started = true
+	if event.is_action_pressed("control_skater"):
+		if not is_started:
+			is_started = true
+			return
+		var closest_hook := hook_detector.get_closest_hook()
+		if closest_hook:
+			connect_to_hook(closest_hook)
+	if event.is_action_released("control_skater"):
+		disconnect_from_hook()
 
-func _physics_process(_delta: float) -> void:
+func connect_to_hook(hook: Hook) -> void:
+	is_connected_to_hook = true
+	hook_connector.visible = true
+	connected_hook_position = hook.global_position
+	connected_hook_distance = global_position.distance_to(connected_hook_position)
+	var perpendicular_vector = (connected_hook_position - global_position).normalized()
+	is_rotating_clockwise = perpendicular_vector.angle_to(direction_vector) < 0
+	direction_vector = perpendicular_vector.rotated(PI/2 * (-1 if is_rotating_clockwise else 1))
+
+func disconnect_from_hook() -> void:
+	is_connected_to_hook = false
+	hook_connector.visible = false
+
+func _physics_process(delta: float) -> void:
 	if not is_started: return
 	
-	velocity = Vector2.RIGHT.rotated(direction_angle) * SPEED
+	if is_connected_to_hook:
+		hook_connector.set_point_position(1, connected_hook_position - global_position)
+		var angular_velocity = SPEED/connected_hook_distance * delta * (1 if is_rotating_clockwise else -1)
+		direction_vector = direction_vector.rotated(angular_velocity)
+	
+	velocity = direction_vector * SPEED
 	
 	move_and_slide()
